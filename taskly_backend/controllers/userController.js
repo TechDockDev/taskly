616ffe5010a6162421061f4906805715ea96cd7f
Sachin Singh <sachinsingh9272@gmail.com>
2025-06-06 13:03:25 +0530

    Implemented User Logout and Delete user

--- taskly_backend/controllers/userController.js
+++ taskly_backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/userModel.js'
+import Task from '../models/taskModel.js';
 import { v2 as cloudinary } from 'cloudinary'
 
 export const Get_Single_User = async (req, res) => {
@@ -21,15 +22,14 @@ export const Get_Single_User = async (req, res) => {
 }
 
 export const Delete_User = async (req, res) => {
-    const userId = req.auth.id;
-    if (!userId) return res.status(404).json({ message: "No valid user find", success: false });
+    // const userId = req.auth.id;
+    const userId = '683a9929eaeb4e3259a3da18';
     try {
-        const userData = await User.findById(userId);
-        if (userData) {
-            const deletedUser = User.deleteOne({ _id: userId });
-            return res.status(200).json({ message: "User deleted successfully", success: true, deletedUser });;
-        }
-        return res.status(404).json({ message: "No user found", success: false });
+        const [deletedTask, deletedUser] = await Promise.all([
+            Task.deleteMany({ userId: userId }),
+            User.deleteOne({ _id: userId })
+        ]);
+        return res.status(200).json({ message: "User deleted successfully", success: true, deletedUser, deletedTask });;
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal Server Error", success: false });
@@ -134,7 +134,7 @@ export const Remove_Tag_From_User = async (req, res) => {
         );
 
         if (!updatedUser) {
-            return res.status(404).json({ message: "User not found", success:false });
+            return res.status(404).json({ message: "User not found", success: false });
         }
 
         res.status(200).json({
@@ -153,13 +153,13 @@ export const Get_All_Tag_Of_User = async (req, res) => {
 
     try {
         const user = await User.findById(userId);
-        if(!user){
-            return res.status(404).json({message:"No user Found", success:false});
+        if (!user) {
+            return res.status(404).json({ message: "No user Found", success: false });
         }
         const tags = user.tags;
-        return res.status(200).json({message:"All Tags", success:true, tags});
+        return res.status(200).json({ message: "All Tags", success: true, tags });
     } catch (error) {
         console.log(error.message);
-        return res.status(500).json({message:"Internal server error", success:false})
+        return res.status(500).json({ message: "Internal server error", success: false })
     }
 }
