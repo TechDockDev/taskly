56c6d7e912851f273cf6501186450166595118d7
Sachin Singh <sachinsingh9272@gmail.com>
2025-09-02 15:51:29 +0530

    Applied morgon for API logs

--- taskly_backend/server.js
+++ taskly_backend/server.js
@@ -3,12 +3,16 @@ import connectDB from './config/db.js';
 import taskRoutes from './routes/taskRoute.js'
 import userRoutes from './routes/userRoute.js'
 import dotenv from 'dotenv';
+import morgon from 'morgan'
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 connectDB();
+
+app.use(morgon(':method :url :status :res[content-length] - :response-time ms'));
+
 app.use('/api/v1/task', taskRoutes);
 app.use('/api/v1/user', userRoutes);
 
